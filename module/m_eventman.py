2117cd66eb5af724be2c9de49dbc7ebc456f1dfd
Tee Age <65055124+T334GE@users.noreply.github.com>
2025-06-18 17:50:31 +0200

    methode zur erstellung eines events

--- module/m_eventman.py
+++ module/m_eventman.py
@@ -18,10 +18,18 @@ class Event_Manager:
         self._event_liste:dict[int:list[list[int]],str,str] = {} # platzhalter, event-liste muss ausgelagert werden.
         #print(len(self._event_liste)) # debug
 
-    def get_event_liste(self) -> list[list[int]|str] or None:
-        for event in self.event_liste:
-            return self.event_liste[event] if self.__chk_event(event) else None
-        return None
+    def event_erstellen(self,event_zeit:dict[str:int],event_akt:str,event_name:str="") -> None:
+        """Fügt ein Event der Liste hinzu. benötigt eine event-zeit (vorher festgelegt),
+        eine event-aktion (aus einer vorgegebenen Liste), und einen event-namen.
+        Dem Event wird eine ID zugeteilt und es wird in der 'Event_Manager._event_liste' gespeichert."""
+        if not self.__chk_event_zeit(event_zeit):
+            raise exception("Zeiten für das Event sind im falschen Format.\n")
+        if type(event_akt) != str or event_akt not in ["klingeln","erinnern","email","sms"]:
+            raise exception("Ungültige Event-Aktion.\n")
+        if type(event_name) != str:
+            raise exception("Ungültige Zeichen für Event-Namen\n")
+        self._event_liste.update({len(self._event_liste):[[event_zeit],event_akt,event_name]})
+        print(type(event_zeit), event_zeit)
 
     def event_erstellen(self,zeit:list[int],aktion:str) -> None:
         pass
