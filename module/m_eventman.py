6be09456f2b05869c701896ca458b832cc51ef15
Tee Age <65055124+T334GE@users.noreply.github.com>
2025-06-21 15:31:58 +0200

    Klassen-Attribute private gesetzt

--- module/m_eventman.py
+++ module/m_eventman.py
@@ -1,9 +1,9 @@
 """
 Eventman
 ————————————
-#Event-Liste:dict{EventID:Event} #Event-Liste im Format {EventID:int: list[datetime, Event-Aktion: str, Event-Name: str]}.
-#System-Zeit:datetime #Aktuelle Systemzeit.
-#Event-Aktionen:list[str] #Liste der verfügbaren Event-Aktionen.
+-Event-Liste:dict{EventID:Event} #Event-Liste im Format {EventID:int: list[datetime, Event-Aktion: str, Event-Name: str]}.
+-System-Zeit:datetime #Aktuelle Systemzeit.
+-Event-Aktionen:list[str] #Liste der verfügbaren Event-Aktionen.
 ————————————
 -init() -> None
 -events_laden() -> None
@@ -26,11 +26,11 @@ class Eventman:
     EVENTS_CSV = 'events.csv'  # Pfad zur CSV-Datei, in der die Events gespeichert werden
     def __init__(self) -> None:
         """Initialisiert die Eventman-Klasse und lädt die Events aus der CSV-Datei."""
-        self._system_zeit = datetime.now() # Aktuelle Systemzeit
-        self._event_liste: dict[int: list] = {} # Event-Liste im Format {EventID:int: list[datetime, Event-Aktion: str, Event-Name: str]}
-        self._event_aktionen: list[str] = ["klingeln", "email", "sms", "anruf", "alarm", "test"] # Liste der verfügbaren Event-Aktionen
+        self.__system_zeit = datetime.now() # Aktuelle Systemzeit
+        self.__event_liste: dict[int: list] = {} # Event-Liste im Format {EventID:int: list[datetime, Event-Aktion: str, Event-Name: str]}
+        self.__event_aktionen: list[str] = ["klingeln", "email", "sms", "anruf", "alarm", "test"] # Liste der verfügbaren Event-Aktionen
         self.__events_laden() # Lädt die Events aus der CSV-Datei
-        for event in self._event_liste.values():
+        for event in self.__event_liste.values():
             print(f"Event-Backlog wird geprüft. Event-Aktion: '{self._event_trigger(event[0], event[1])}'\n")  # Überprüft, ob die Events bereits ausgelöst werden sollten
 
     @property
@@ -38,22 +38,22 @@ class Eventman:
         """Gibt die aktuelle Systemzeit zurück.
         :return:datetime # Aktuelle Systemzeit als datetime-Objekt.
         """
-        return datetime.now()
+        return self.__system_zeit
 
     @property
     def event_liste(self) -> dict[int:list]:
         """Gibt die Event-Liste zurück."""
-        return self._event_liste
+        return self.__event_liste
 
     @event_liste.setter
-    def event_liste(self, new_event_liste:dict[int:list]) -> None:
+    def event_liste(self, new__event_liste:dict[int:list]) -> None:
         """Setzt eine neue Event-Liste und speichert sie in CSV.
-        :param new_event_liste:dict[int: list[datetime, str, str]] #Neue Event-Liste im Format {EventID:int: list[datetime, Event-Aktion: str, Event-Name: str]}
+        :param new__event_liste:dict[int: list[datetime, str, str]] #Neue Event-Liste im Format {EventID:int: list[datetime, Event-Aktion: str, Event-Name: str]}
         :raises exception: Bei falschem Typ der neuen Event-Liste.
         """
-        if not isinstance(new_event_liste, dict):
+        if not isinstance(new__event_liste, dict):
             raise Exception("Neue Event-Liste muss vom Typ 'dict' sein.\n")
-        self._event_liste = new_event_liste
+        self.__event_liste = new__event_liste
         self.__events_speichern()
 
     @property
@@ -62,19 +62,19 @@ class Eventman:
         :return:list[str] #Liste der verfügbaren Event-Aktionen.
         :raises exception: Bei leerer Event-Aktionsliste.
         """
-        if not self._event_aktionen:
+        if not self.__event_aktionen:
             raise Exception("Aktions-Liste ist leer.\n")
-        return self._event_aktionen
+        return self.__event_aktionen
 
     @event_aktionen.setter
-    def event_aktionen(self, new_event_aktionen:list[str]) -> None:
+    def event_aktionen(self, _new_event_aktionen:list[str]) -> None:
         """Setzt eine neue Liste von Event-Aktionen.
-        :param new_event_aktionen:list[str] #Neue Liste der verfügbaren Event-Aktionen.
+        :param _new_event_aktionen:list[str] #Neue Liste der verfügbaren Event-Aktionen.
         :raises exception: Bei falschem Typ der neuen Event-Aktionsliste.
         """
-        if not isinstance(new_event_aktionen, list) or not all(isinstance(a, str) for a in new_event_aktionen):
+        if not isinstance(_new_event_aktionen, list) or not all(isinstance(a, str) for a in _new_event_aktionen):
             raise Exception("Aktions-Liste muss eine Liste von Strings sein.\n")
-        self._event_aktionen = new_event_aktionen
+        self.__event_aktionen = _new_event_aktionen
 
     def __events_laden(self) -> None:
         """Lädt die Events aus der CSV-Datei in die Event-Liste.
@@ -84,7 +84,7 @@ class Eventman:
             with open(self.EVENTS_CSV, 'r', encoding='utf-8') as f:
                 csv_reader = reader(f)
                 next(csv_reader)
-                self._event_liste = {
+                self.__event_liste = {
                     int(row[0]): [datetime.fromisoformat(row[1]), row[2], row[3].strip()]
                     for row in csv_reader if row
                 }
@@ -98,7 +98,7 @@ class Eventman:
         with open(self.EVENTS_CSV, 'w', newline='', encoding='utf-8') as f:
             csv_writer = writer(f)
             csv_writer.writerow(['EventID', 'Zeitstempel', 'Aktion', 'Name'])
-            for event_id, event_data in self._event_liste.items():
+            for event_id, event_data in self.__event_liste.items():
                 csv_writer.writerow([event_id, event_data[0].isoformat(), event_data[1], event_data[2]])
 
     def _event_trigger(self, event_zeit: datetime, event_akt: str) -> str:
@@ -110,7 +110,7 @@ class Eventman:
         :raises exception: Bei ungültiger Event-Zeit oder Aktion."""
         if not isinstance(event_zeit, datetime):
             raise Exception("Event-Zeit muss ein datetime-Objekt sein.\n")
-        if not isinstance(event_akt, str) or event_akt not in self._event_aktionen:
+        if not isinstance(event_akt, str) or event_akt not in self.__event_aktionen:
             raise Exception("Keine gültige Aktion.\n")
         while True:
             jetzt = datetime.now()
@@ -129,22 +129,22 @@ class Eventman:
             raise Exception("Event-Zeit muss ein datetime-Objekt sein.\n")
         if event_zeit < self.system_zeit - timedelta(seconds=30):
             raise Exception("Event-Zeit darf nicht in der Vergangenheit liegen.\n")
-        if not isinstance(event_akt, str) or event_akt not in self._event_aktionen:
+        if not isinstance(event_akt, str) or event_akt not in self.__event_aktionen:
             raise Exception("Ungültige Event-Aktion.\n")
         if not isinstance(event_name, str):
             raise Exception("Event-Name muss aus String bestehen.\n")
         if event_name.strip() == "":
             raise Exception("Event-Name darf nicht leer sein.\n")
-        if self._event_liste:
-            new_event_id = max(self._event_liste.keys()) + 1 # Neue Event-ID basierend auf der höchsten vorhandenen ID
+        if self.__event_liste:
+            new_event_id = max(self.__event_liste.keys()) + 1 # Neue Event-ID basierend auf der höchsten vorhandenen ID
         else:
             new_event_id = 1 # Startet bei 1, wenn keine Events vorhanden sind
-        self._event_liste[new_event_id] = [event_zeit, event_akt, event_name] # Fügt das neue Event der Liste hinzu
+        self.__event_liste[new_event_id] = [event_zeit, event_akt, event_name] # Fügt das neue Event der Liste hinzu
         print(f"Event '{event_name}' wurde erstellt mit ID '{new_event_id}'.\n")
         try:# Speichert das neue Event in der CSV-Datei
             self.__events_speichern()
         except Exception as e:
-            del self._event_liste[new_event_id]
+            del self.__event_liste[new_event_id]
             raise Exception(f"Fehler beim Speichern des Events: {str(e)}\n")
 
     def event_aufrufen(self, event_id: int) -> list:
@@ -154,8 +154,8 @@ class Eventman:
         :raises exception: Bei ungültiger Event-ID oder wenn das Event nicht gefunden wird.
         """
         try:
-            if event_id in self._event_liste:
-                return self._event_liste[event_id]
+            if event_id in self.__event_liste:
+                return self.__event_liste[event_id]
             # Wenn nicht im Cache, dann aus CSV laden
             with open(self.EVENTS_CSV, 'r', encoding='utf-8') as f:
                 csv_reader = reader(f)
@@ -172,9 +172,9 @@ class Eventman:
         :param event_id:int # ID-Nummer des Events
         :raises exception: bei ungültiger Event-ID oder wenn das Event nicht gefunden wird.
         """
-        if event_id not in self._event_liste:
+        if event_id not in self.__event_liste:
             raise Exception(f"Es existiert kein Event mit der ID '{event_id}'.\n")
-        del self._event_liste[event_id]
+        del self.__event_liste[event_id]
         print(f"Event mit ID {event_id} wurde entfernt.\n")
         try:
             self.__events_speichern()
