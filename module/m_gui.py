b48f16a8b9ac0f22ae9f64711a6e600301abff42
Tee Age <65055124+T334GE@users.noreply.github.com>
2025-07-06 23:52:32 +0200

    erste 'funktionen'

--- module/m_gui.py
+++ module/m_gui.py
@@ -1,4 +1,10 @@
+"""
+Hauptmodul des GUIs. Hier wird die App gestartet und das Layout des Home-Bildschirms definiert.
+"""
+
 import kivy
+from kivy.clock import Clock
+from kivy.properties import StringProperty
 from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
@@ -11,15 +17,39 @@ class CalendrumApp(MDApp):
     Ruft beim Aufruf dieses Objekts die eigene "build"-Methode auf,
     die das Layout der App erstellt.
     """
+    uhrzeit = StringProperty()  # Uhrzeit wird als StringProperty definiert, um sie im KV-Layout zu verwenden.
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.__zeit = Datumzeit()# Initialisiert die Zeit- und Datumsobjekte.
-        self.zeit.jetzt()# Aktuelles Datum und Uhrzeit werden gesetzt
+        self.zeit = Datumzeit()# Initialisiert die Zeit- und Datumsobjekte.
+        self.zeit.jetzt()
+        self.uhrzeit = f"{self.zeit.stunde:02d}:{self.zeit.minute:02d}:{self.zeit.sekunde:02d} Uhr"
+        self.monate_deutsch = ["Januar", "Februar", "März", "April", "Mai", "Juni","Juli", "August", "September", "Oktober", "November", "Dezember"]
 
     @property
-    def zeit(self) -> Datumzeit:
-        """Gibt das Datumzeit-Objekt zurück."""
-        return self.__zeit
+    def home_screen(self) -> MDScreen:
+        """Gibt den HomeScreen zurück."""
+        return self.root.get_screen("home")
+
+    def update_monat(self) -> None:
+        """Methode zum Updaten des Monats im HomeScreen beim Klicken des Buttons."""
+        try:
+            self.home_screen.ids.monat_anzeige.text = self.monate_deutsch[self.zeit.get_monat() - 1]
+        except Exception as e:
+            print(f"Error updating month: {e}")
+
+    def update_jahr(self) -> None:
+        """Methode zum Updaten des Jahres im HomeScreen beim Klicken des Buttons."""
+        try:
+            current_year = self.zeit.get_jahr()
+            if current_year is not None:
+                self.home_screen.ids.jahr_anzeige.text = str(current_year)
+        except Exception as e:
+            print(f"Error updating year: {e}")
+
+    def update_uhrzeit(self, *args) -> None:
+        """Aktualisiert die Uhrzeit im HomeScreen jede Sekunde."""
+        self.zeit.jetzt()
+        self.uhrzeit = f"{self.zeit.stunde:02d}:{self.zeit.minute:02d}:{self.zeit.sekunde:02d} Uhr"
 
     def build(self) -> MDScreenManager:
         """Wird automatisch aufgerufen, wenn die App gestartet wird.
@@ -29,20 +59,12 @@ class CalendrumApp(MDApp):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Gray"
 
+        Clock.schedule_interval(self.update_uhrzeit, 1) # Aktualisiert die Zeit jede Sekunde
+
         return Manager()
 
-    def update_jahr(self) -> None:
-        """Methode zum Updaten des Jahres im HomeScreen beim Klicken des Buttons."""
-        home_screen = self.root.get_screen("home")
-        home_screen.ids.jahr_auswahl.text = str(self.zeit.jahr)
 
-    def update_monat(self) -> None:
-        """Methode zum Updaten des Monats im HomeScreen beim Klicken des Buttons."""
-        home_screen = self.root.get_screen("home")
-        home_screen.ids.monat_auswahl.text = str(self.zeit.monat)
-
-
-"""Folgende Klassen müssen in der py definiert werden, 
+"""Folgende Klassen müssen in der .py definiert werden, 
 damit sie in der .kv-Datei verwendet werden können.
 Funktioniert auch ohne super()-Konstruktor, vielleicht aber nicht in allen Fällen."""
 
@@ -54,10 +76,7 @@ class HomeScreen(MDScreen):
     """Startbildschirm der App."""
     pass
 
-
-
-
-
+"""Main Klasse OOP"""
 
 class Main:
     """Startet die App.
