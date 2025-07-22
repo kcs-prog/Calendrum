431dd7d56ab066aaf97445be78257ba288c6a245
Tee Age <65055124+T334GE@users.noreply.github.com>
2025-07-22 11:53:43 +0200

    Main-Klasse angepasst

--- module/main.py
+++ module/main.py
@@ -1,5 +1,11 @@
 from m_gui import CalendrumApp
 
 class Main:
-    app:CalendrumApp = CalendrumApp()
-    app.run()
+    def __init__(self):
+        self.__app:CalendrumApp = CalendrumApp()
+    @property
+    def app(self)->CalendrumApp:
+        return self.__app
+
+if __name__ == "__main__":
+    Main().app.run()
