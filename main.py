9cf89ef34761b53aa21f1b9a3d2df0a7b867f589
Trung H. Nguyen <teeageing@gmail.com>
2025-07-24 08:26:16 +0200

    Ordnerstruktur angepasst an übliche Struktur

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from m_gui import CalendrumApp
+from scripts.m_gui import CalendrumApp
 
 class Main:
     def __init__(self):
         self.__app:CalendrumApp = CalendrumApp()
     @property
-    def app(self)->CalendrumApp:
+    def app(self) -> CalendrumApp:
         return self.__app
 
 if __name__ == "__main__":
