a44147a522b04aa1ebeebbef683ef7e2d3f7dcd4
Trung H. Nguyen <teeageing@gmail.com>
2025-08-11 11:55:23 +0200

    zeitverschiebung angepasst

--- scripts/m_eventman.py
+++ scripts/m_eventman.py
@@ -134,18 +134,21 @@ class Eventman:
                         if ev.jaehrlich:
                             neue_zeit.jahr += 1
                         elif ev.monatlich:
-                            if neue_zeit.monat >= 12:
+                            if not neue_zeit.monat >= 12:
+                                neue_zeit.monat += 1
+                            else:
                                 neue_zeit.monat = 1
                                 neue_zeit.jahr += 1
-                            else: neue_zeit.monat += 1
                         elif ev.taeglich:
-                            if neue_zeit.tag >= neue_zeit.max_tage(neue_zeit.monat, neue_zeit.jahr):
+                            if not neue_zeit.tag >= neue_zeit.max_tage(neue_zeit.monat, neue_zeit.jahr):
+                                neue_zeit.tag += 1
+                            else:
                                 neue_zeit.tag = 1
-                                if neue_zeit.monat >= 12:
+                                if not neue_zeit.monat >= 12:
+                                    neue_zeit.monat += 1
+                                else:
                                     neue_zeit.monat = 1
                                     neue_zeit.jahr += 1
-                                else: neue_zeit.monat += 1
-                            else: neue_zeit.tag += 1
                         ev.zeit = neue_zeit
                     self.event_entfernen(ev.id) if not ev.taeglich and not ev.monatlich and not ev.jaehrlich else None
                     self.__events_speichern()
